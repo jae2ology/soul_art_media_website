6c13ab8bf1ffce82d86a134ff748f617eb7e94f3
jae2ology <jacksonjae90@gmail.com>
2026-03-18 22:01:50 -0400

    updated img links

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,32 +1,38 @@
 import React from 'react';
+import soul from './assets/soul.png';
+import about from './assets/about.png';
+import pieces from './assets/pieces.png';
+import books from './assets/books.png';
+import prints from './assets/prints.png';
+import contact from './assets/img_3.png'
 
 export default function Home() {
     return (
         <div className={"flex flex-col items-center justify-center min-h-screen bg-[#D6BCC8] p-8"}>
 
             <div className={"mb-12 md:mb-20"}>
-                <img src={"src/assets/soul.png"} />
+                <img src={soul} />
             </div>
 
             <div className={"flex flex-wrap justify-center gap-6 md:gap-12"}>
                 <a href={'/about'} className="hover:scale-105 transition-transform">
-                <img src={"src/assets/about.png"} />
+                <img src={about} />
                 </a>
 
                 <a href={'/pieces'} className="hover:scale-105 transition-transform">
-                    <img src={"src/assets/pieces.png"} />
+                    <img src={pieces} />
                 </a>
 
                 <a href={'/prints'} className="hover:scale-105 transition-transform">
-                    <img src={"src/assets/prints.png"} />
+                    <img src={prints} />
                 </a>
 
                 <a href={'/books'} className="hover:scale-105 transition-transform">
-                    <img src={"src/assets/books.png"} />
+                    <img src={books} />
                 </a>
 
                 <a href={'/contact'} className="hover:scale-105 transition-transform">
-                    <img src={"src/assets/img_3.png"} />
+                    <img src={contact} />
                 </a>
 
             </div>
